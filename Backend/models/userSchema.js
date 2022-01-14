91cb32c0f193f5ad3fe7ce52480d162d5bcd2560
Parul <parulsurana28@gmail.com>
2022-01-14 11:06:52 +0530

    Applied Password Hashing

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -25,6 +26,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+
+//Hashing Password
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    }
+    next();
 })
 
 const User = mongoose.model('USER', userSchema);
--- Backend/router/auth.js
+++ Backend/router/auth.js
@@ -52,14 +52,15 @@ router.post('/register', async(req,res) => {
         const userExist =  await User.findOne({email : email});
         if(userExist){
             return res.status(422).json({error: "Email already exists"});
+        }else if(password != cpassword){
+            return res.status(422).json({error: "Passwords are not matching"});
+        }else{
+            const user = new User({name, email, phone, work, password, cpassword});
+            // pre wala will work.(passwor hashing - UserSchema)
+            await user.save();
+    
+            res.status(201).json({message : "User Registered Successfully"});
         }
-        
-        const user = new User({name, email, phone, work, password, cpassword});
-
-        await user.save();
-
-        res.status(201).json({message : "User Registered Successfully"});
-
    }catch(err){
      console.log(err);
    }  
