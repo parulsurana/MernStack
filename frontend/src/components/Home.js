2b8cc018e20f7fc24be6885013b725cd2db26882
Parul <parulsurana28@gmail.com>
2022-01-15 11:10:05 +0530

    Created Navbar

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <p>WELCOME</p>
+            <h1> We are THE MERN DEVELOPER!</h1>
+        </div>
+    )
+}
