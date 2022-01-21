5a8f3c3c1fd6faf31bf57286109a2b4a50374061
Parul <parulsurana28@gmail.com>
2022-01-21 19:48:45 +0530

    Setup the Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
+import React from "react";
+// import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  return (
-   <>
-      <Navbar />
-   </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Router>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/signup' element={<Signup />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,46 +1,60 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <>
+	return (
+		<>
+			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+				<a className='navbar-brand' href='#'>
+					Navbar
+				</a>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon'></span>
+				</button>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav ms-auto'>
+						<li className='nav-item active'>
+							<a className='nav-link' href='/'>
+								Home
+							</a>
+						</li>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<li className='nav-item'>
+							<a className='nav-link' href='/about'>
+								About
+							</a>
+						</li>
 
-    <ul class="navbar-nav ms-auto">
+						<li className='nav-item'>
+							<a className='nav-link' href='/contact'>
+								Contact
+							</a>
+						</li>
 
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
+						<li className='nav-item'>
+							<a className='nav-link' href='/login'>
+								Login
+							</a>
+						</li>
 
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Registration</a>
-      </li>
-
-    </ul>
-    
-  </div>
-</nav>
-
-        </>
-
-    )
+						<li className='nav-item'>
+							<a className='nav-link' href='/signup'>
+								Registration
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
 }
