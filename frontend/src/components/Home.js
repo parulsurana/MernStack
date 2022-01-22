646a61a20f165968f8ca9d025b07c5c2d1768f16
Parul <parulsurana28@gmail.com>
2022-01-22 12:26:59 +0530

    Create Home Page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <p className='pt-5'>WELCOME HOME</p>
-            <h1> We are THE MERN DEVELOPER</h1>
-        </div>
-    )
+	return (
+		<>
+			<div className='home-page'>
+				<div className='home-div'>
+					<p className='pt-5'>WELCOME HOME</p>
+					<h1> We are THE MERN DEVELOPER</h1>
+				</div>
+			</div>
+		</>
+	);
 }
